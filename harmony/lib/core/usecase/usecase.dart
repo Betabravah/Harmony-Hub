8f9aea38e10241be6a4a7e1d18b763f915a9aff9
betabravah <betabravah@gmail.com>
2023-09-04 10:06:48 +0300

    Add Usecase Abstarct Class

---
+++ harmony/lib/core/usecase/usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../error/failures.dart';
+
+abstract class Usecase<T, Params> {
+  Future<Either<Failure, T>> call(Params params);
+}
+
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
+}
